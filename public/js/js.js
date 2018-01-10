8548cb53247ad945803935b19f8038e276466f01
Phill Scargill <phill.scargill@gmail.com>
2018-01-10 11:53:48 +0000

    Graphical Change
    Updated Site Background pattery and colour scheme

--- public/js/js.js
+++ public/js/js.js
@@ -65,7 +65,7 @@ $(function () {
                     $("#pagewrapper").prepend(clone);
                 }
             }
-            $("#titlebartext").text("Low Priority Work Orders - " + $('div.jobdivouterclass').length);
+            $("#titlebartext").text("Low Priority Work Orders - " + ($('div.jobdivouterclass').length-1));
             $("#JobID" + JobID + " > #jobdivinner > #JobTitle").text("Work Order: " + JobID);
             $("#JobID" + JobID + " > #jobdivinner > #JobDesc").text(JobDesc);
             $("#JobID" + JobID + " > #jobdivinner > #JobTime").text(JobDateTime);
@@ -73,83 +73,28 @@ $(function () {
             $("#JobID" + JobID + " > #jobdivinner > #JobEntity").text(JobEntity);
             if (JobStatusID == 1) {
                 $("#JobID" + JobID + " > #jobdivinner").css({
-                    'box-shadow': "0px 0px 0px 3px rgba(209,0,0,1)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "rgb(248,80,50)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "-webkit-linear-gradient(top, rgba(248,80,50,1) 0%,rgba(241,111,92,1) 50%,rgba(246,41,12,1) 52%,rgba(240,47,23,1) 71%,rgba(231,56,39,1) 100%)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "-linear-gradient(to bottom, rgba(248,80,50,1) 0%,rgba(241,111,92,1) 50%,rgba(246,41,12,1) 52%,rgba(240,47,23,1) 71%,rgba(231,56,39,1) 100%)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "-webkit-linear-gradient(top, rgba(248,80,50,1) 0%,rgba(241,111,92,1) 50%,rgba(246,41,12,1) 52%,rgba(240,47,23,1) 71%,rgba(231,56,39,1) 100%)"
+                    background: "rgb(255, 186, 186)",
+                    color: "rgb(216, 0, 12)"
                 });
             } else if (JobStatusID == 2) {
                 $("#JobID" + JobID + " > #jobdivinner").css({
-                    'box-shadow': "0px 0px 0px 3px rgba(248,80,50, 1)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "rgb(247,204,86)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "-moz-linear-gradient(top, rgba(247,204,86,1) 0%, rgba(244,146,9,1) 50%, rgba(242,109,38,1) 53%, rgba(247,157,74,1) 100%)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "-webkit-linear-gradient(top, rgba(247,204,86,1) 0%,rgba(244,146,9,1) 50%,rgba(242,109,38,1) 53%,rgba(247,157,74,1) 100%)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "background: linear-gradient(to bottom, rgba(247,204,86,1) 0%,rgba(244,146,9,1) 50%,rgba(242,109,38,1) 53%,rgba(247,157,74,1) 100%)"
+                    background: "rgb(255, 193, 86)",
+                    color: "rgb(196, 91, 0)"
                 });
             } else if (JobStatusID == 3) {
                 $("#JobID" + JobID + " > #jobdivinner").css({
-                    'box-shadow': "0px 0px 0px 3px rgba(226,196,0, 1)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "rgb(241,249,87)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "-moz-linear-gradient(top, rgba(241,249,87,1) 0%, rgba(239,239,38,1) 50%, rgba(229,203,9,1) 54%, rgba(238,247,74,1) 100%)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "-webkit-linear-gradient(top, rgba(241,249,87,1) 0%,rgba(239,239,38,1) 50%,rgba(229,203,9,1) 54%,rgba(238,247,74,1) 100%)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "linear-gradient(to bottom, rgba(241,249,87,1) 0%,rgba(239,239,38,1) 50%,rgba(229,203,9,1) 54%,rgba(238,247,74,1) 100%)"
+                    background: "rgb(254, 239, 179)",
+                    color: "rgb(159, 96, 0)"
                 });
             } else if (JobStatusID == 4) {
                 $("#JobID" + JobID + " > #jobdivinner").css({
-                    'box-shadow': "0px 0px 0px 3px rgba(127, 168, 38, 1)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "rgb(191,210,85)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "-moz-linear-gradient(top, rgba(191,210,85,1) 0%, rgba(142,185,42,1) 50%, rgba(114,170,0,1) 52%, rgba(158,203,45,1) 100%)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "-webkit-linear-gradient(top, rgba(191,210,85,1) 0%,rgba(142,185,42,1) 50%,rgba(114,170,0,1) 52%,rgba(158,203,45,1) 100%)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "linear-gradient(to bottom, rgba(191,210,85,1) 0%,rgba(142,185,42,1) 50%,rgba(114,170,0,1) 52%,rgba(158,203,45,1) 100%)"
+                    background: "rgb(223, 242, 191)",
+                    color: "rgb(79, 138, 16)"
                 });
             } else if (JobStatusID == 5) {
                 $("#JobID" + JobID + " > #jobdivinner").css({
-                    'box-shadow': "0px 0px 0px 3px rgba(136, 148, 155, 1)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "rgb(242,246,248)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "-moz-linear-gradient(top, rgba(242,246,248,1) 0%, rgba(216,225,231,1) 50%, rgba(181,198,208,1) 52%, rgba(224,239,249,1) 100%)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "-webkit-linear-gradient(top, rgba(242,246,248,1) 0%,rgba(216,225,231,1) 50%,rgba(181,198,208,1) 52%,rgba(224,239,249,1) 100%)"
-                });
-                $("#JobID" + JobID + " > #jobdivinner").css({
-                    background: "linear-gradient(to bottom, rgba(242,246,248,1) 0%,rgba(216,225,231,1) 50%,rgba(181,198,208,1) 52%,rgba(224,239,249,1) 100%)"
+                    background: "rgb(237, 237, 237)",
+                    color: "rgb(79, 79, 79)"
                 });
             }
             $('#JobID' + JobID).fadeIn("slow", function () {});
